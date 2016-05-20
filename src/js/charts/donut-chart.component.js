0615dd821841b3303bb1fb79b645786f6564f73e
Ross Hendry <rhendry@scottlogic.co.uk>
2016-05-20 12:27:02 +0100

    Now with rolling text

--- src/js/charts/donut-chart.component.js
+++ src/js/charts/donut-chart.component.js
@@ -57,6 +57,10 @@ function pieChartFactory() {
                         .innerRadius(radius * 0.9)
                         .outerRadius(radius * 0.9);
 
+      const valueLabelArc = d3.svg.arc()
+                              .innerRadius(radius * 0.6)
+                              .outerRadius(radius * 0.6);
+
       const pie = d3.layout.pie()
                     .value(_.property('value'))
                     .sort((a, b) => d3.ascending(a.label, b.label));
@@ -72,10 +76,49 @@ function pieChartFactory() {
       container.append('g').attr('class', 'slices');
       container.append('g').attr('class', 'labels');
       container.append('g').attr('class', 'lines');
+      container.append('g').attr('class', 'values');
 
       addSlices(was, is, data);
+      addValues(was, is, data);
       addTextLabels(was, is, data);
       addPolylines(was, is, data);
+
+      function addValues(was, is, data) {
+        let text = svg.select('.values').selectAll('text').data(pie(was), key);
+
+        text.enter()
+              .append('text')
+              .attr('class', 'valueLabel')
+              .attr('dy', '0.35em')
+              .style('opacity', 0)
+              .text((d) => d.data.value)
+              .each(function(d) {
+                this._current = d;
+              });
+
+        text = svg.select('.values').selectAll('text').data(pie(is), key);
+
+        text.transition().duration(1000)
+            .style('opacity', (d) => d.data.value == 0 ? 0 : 1)
+            .tween('text', function(d) {
+              var i = d3.interpolate(this.textContent, d.data.value);
+              return function(t) {
+                this.textContent = Math.round(i(t));
+              };
+            })
+            .attrTween('transform', function(d) {
+              const interpolate = d3.interpolate(this._current, d);
+              return (t) => {
+                const d2 = interpolate(t);
+                this._current = d2;
+                return 'translate(' + valueLabelArc.centroid(d2) + ')';
+              }
+            });
+
+        text = svg.select('.values').selectAll('text').data(pie(data), key);
+
+        text.exit().transition().delay(1000).remove();
+      }
 
       function addPolylines(was, is, data) {
         let lines = svg.select('.lines').selectAll('polyline').data(pie(was), key);
